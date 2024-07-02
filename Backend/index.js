69527013857d6e479c3c989abea24f695218ca91
Offensic <ziakhan0537@gmail.com>
2024-07-02 06:45:29 -0700

    Cookies not send on Deployment to the Backend

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,7 @@ app.use(cookieparser())
 
 // Allow to Receive  aReques to the Frontend
 const cors = require('cors');
+
 // using Cors for Prodcution
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
